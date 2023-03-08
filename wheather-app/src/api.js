12e78a40c79829da16fbd392c9bfc81d6c5ee299
fayess3 <91080536+fayess3@users.noreply.github.com>
2023-03-08 13:48:57 +0100

    ajout de l'api

---
+++ wheather-app/src/api.js
@@ -0,0 +1,12 @@
+
+export const goeApiOptions = {
+    method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4c5ecfa4f4msh685e326f93c946ep187c0bjsn0752b46e671a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+    };
+    
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo" ;
+
+    
